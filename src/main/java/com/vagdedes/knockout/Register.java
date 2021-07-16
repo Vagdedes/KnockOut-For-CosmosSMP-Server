4a348736efab64bc0b7ad10bcf75bd80a34ffec2
Evangelos <contact@vagdedes.com>
2021-07-16 17:09:43 +0300

    Initial commit

---
+++ src/main/java/com/vagdedes/knockout/Register.java
@@ -0,0 +1,14 @@
+package com.vagdedes.knockout;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Register extends JavaPlugin {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+}
